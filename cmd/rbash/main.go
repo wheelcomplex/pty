56b254cfa177cddf2e652c87e2d7fbc95c4a77ff
David Mortals <david@mortals.city>
2019-06-20 12:48:02 +1200

    gobash: execute binary arg when pass to bash

--- cmd/rbash/main.go
+++ cmd/rbash/main.go
@@ -1,3 +1,5 @@
+// run first aviable shell with pty
+
 package main
 
 import (
@@ -7,50 +9,126 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 	"syscall"
 
+	"github.com/wheelcomplex/filetype"
 	"github.com/wheelcomplex/permbits"
 	"github.com/wheelcomplex/pty"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-func test() error {
+func getFileType(path string) (ct string, err error) {
+
+	ct = "application/octet-stream"
+	permissions, err := permbits.Stat(path)
+	if err != nil {
+		// log.Printf("file %s stat error: %s\n", path, err)
+		return ct, err
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		return ct, err
+	}
+	defer file.Close()
+
+	// Only the first 512 bytes are used to sniff the content type.
+	buffer := make([]byte, 512)
+	_, err = file.Read(buffer)
+	if err != nil {
+		return ct, err
+	}
+
+	kind, _ := filetype.Match(buffer)
+	log.Printf("%s is identify as %s, %s by filetype.Match\n", path, kind.Extension, kind.MIME.Value)
+
+	if kind.Extension == "elf" && kind.MIME.Value == "application/x-executable" && permissions.UserExecute() {
+		return "application/x-elf-executable", nil
+	}
+	return ct, nil
+}
+
+func runshell() error {
 	// Create arbitrary command.
 
 	log.SetOutput(os.Stderr)
 	log.SetPrefix(" - initz, ")
 
-	shell := "/bin/bash"
-
-	permissions, err := permbits.Stat(shell)
-	if err != nil {
-		log.Printf("file %s stat error: %s, try next ...\n", shell, err)
-	} else {
-		if permissions.UserExecute() == false {
-			err = fmt.Errorf("%s is not executable\n", shell)
-		} else if permissions.UserRead() == false {
-			err = fmt.Errorf("%s is not readable\n", shell)
-		}
+	shells := [...]string{
+		"/bin/bash",
+		"/root/bin/bash",
+		"/bin/sh",
+		"/root/bin/sh",
 	}
-
-	if err != nil {
-		shell = "/root/bin/bash"
-		permissions, err = permbits.Stat(shell)
+	shell := ""
+	var shellErr error = nil
+	for _, shell = range shells {
+		permissions, err := permbits.Stat(shell)
 		if err != nil {
-			log.Printf("file %s stat error: %s, exit ...\n", shell, err)
-			return err
+			log.Printf("file %s stat error: %s, try next ...\n", shell, err)
+		} else {
+			if permissions.UserExecute() == false {
+				err = fmt.Errorf("%s is not executable\n", shell)
+			} else if permissions.UserRead() == false {
+				err = fmt.Errorf("%s is not readable\n", shell)
+			}
 		}
-		if permissions.UserExecute() == false {
-			err = fmt.Errorf("%s is not executable\n", shell)
-			return err
-		} else if permissions.UserRead() == false {
-			err = fmt.Errorf("%s is not readable\n", shell)
-			return err
+		shellErr = err
+		if err == nil {
+			break
 		}
 	}
-	log.Printf("SHELL: %s\n", shell)
 
-	c := exec.Command(shell)
+	if shellErr != nil {
+		log.Printf("shells (%d) not found, exit ...\n", len(shells))
+		return shellErr
+	}
+
+	cmdline := ""
+	for _, arg := range os.Args[1:] {
+		cmdline += arg + " "
+	}
+	cmdline = strings.Trim(cmdline, " ")
+
+	var firstArg string = ""
+	if len(cmdline) > 0 {
+		log.Printf("SHELL: %s %s\n", shell, cmdline)
+		firstArg = strings.Trim(os.Args[1], " ")
+	} else {
+		log.Printf("SHELL: %s\n", shell)
+	}
+
+	// lookup
+	path, err := exec.LookPath(firstArg)
+	if err != nil {
+		// log.Printf("%s not found in PATH\n", firstArg)
+		firstArg = ""
+	} else {
+		// log.Printf("%s found in PATH: %s\n", firstArg, path)
+
+		// check for script file
+		fileType, err := getFileType(path)
+
+		if err == nil && fileType == "application/x-elf-executable" {
+			firstArg = path
+		} else {
+			firstArg = ""
+		}
+	}
+
+	var c *exec.Cmd
+
+	if len(firstArg) > 0 {
+		// binary file, execute directly
+		if len(os.Args) >= 3 {
+			c = exec.Command(firstArg, os.Args[2:]...)
+		} else {
+			c = exec.Command(firstArg)
+		}
+	} else {
+		// script file, execute by shell
+		c = exec.Command(shell, os.Args[1:]...)
+	}
 
 	// Start the command with a pty.
 	ptmx, err := pty.Start(c)
@@ -87,7 +165,7 @@ func test() error {
 }
 
 func main() {
-	if err := test(); err != nil {
+	if err := runshell(); err != nil {
 		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/wheelcomplex/pty
 go 1.12
 
 require (
+	github.com/wheelcomplex/filetype v1.0.9
 	github.com/wheelcomplex/permbits v0.0.0-20190612203442-39d7c581d2ee
 	golang.org/x/crypto v0.0.0-20190618222545-ea8f1a30c443
 )
